4306aa6473ff0342b7b6791f5695a756e4832310
Hud Wahab <hudwahab@gmail.com>
2022-04-04 07:56:14 -0600

    corrected

--- 01-composition-inheritance/exercise.py
+++ 01-composition-inheritance/exercise.py
@@ -1,6 +1,5 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from enum import Enum, auto
-from typing import Protocol
 
 
 class FuelType(Enum):
@@ -14,36 +13,7 @@ class TruckCabStyle(Enum):
     EXTENDED = auto()
     CREW = auto()
 
-@dataclass
-class Price(Protocol):
 
-    def compute_price(self) -> int:
-        ...
-
-@dataclass
-class PricePerKm(Price):
-    price_per_km: int
-    km_driven: int
-
-    def compute_price(self) -> int:
-        return (self.price_per_km * self.km_driven)
-
-@dataclass
-class PricePerDay(Price):
-    price_per_day: int
-    days_rented: int
-
-    def compute_price(self) -> int:
-        return (self.price_per_day * self.days_rented)
-
-@dataclass
-class PricePerMonth(Price):
-    price_per_month: int
-    months_rented: int
-
-    def compute_price(self) -> int:
-        return (self.price_per_month * self.months_rented)
-    
 @dataclass
 class Vehicle:
     brand: str
@@ -52,17 +22,34 @@ class Vehicle:
     fuel_type: FuelType
     license_plate: str
     reserved: bool
-    price: list[Price] = field(default_factory=list)
 
 
 @dataclass
-class Car(Vehicle):
-    number_of_seats: int = 5
-    storage_capacity_litres: int = 100
+class VehiclePerDay(Vehicle):
+    price_per_km: int
+    price_per_day: int
+
+
+@dataclass
+class VehiclePerMonth(Vehicle):
+    price_per_month: int
+
+
+@dataclass
+class CarPerDay(VehiclePerDay):
+    number_of_seats: int
+    storage_capacity_litres: int
+
+
+@dataclass
+class CarPerMonth(VehiclePerMonth):
+    number_of_seats: int
+    storage_capacity_litres: int
+
 
 @dataclass
 class Truck(Vehicle):
-    cab_style: TruckCabStyle = TruckCabStyle.REGULAR
+    cab_style: TruckCabStyle
 
 
 @dataclass
@@ -70,13 +57,13 @@ class Trailer:
     brand: str
     model: str
     capacity_m3: int
+    price_per_month: int
     reserved: bool
-    price: list[Price] = field(default_factory=list)
 
 
 def main():
     pass
-    
+
 
 if __name__ == "__main__":
     main()
