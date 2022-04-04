bc22ad3bb6b83d503657fc4b6b3b4658732ed7b9
Hud Wahab <hudwahab@gmail.com>
2022-04-04 07:29:33 -0600

    corrected

--- 03-low-coupling/order_before.py
+++ 03-low-coupling/order_before.py
@@ -13,29 +13,29 @@ class Order:
         self.items: list[str] = []
         self.quantities: list[int] = []
         self.prices: list[int] = []
-        self.status: PaymentStatus = PaymentStatus.OPEN
+        self.status: str = "open"
 
 
-    def add_item(self, name: str, quantity: int, price: int) -> None:
-        self.items.append(name)
-        self.quantities.append(quantity)
-        self.prices.append(price)
+def add_item(order: Order, name: str, quantity: int, price: int) -> None:
+    order.items.append(name)
+    order.quantities.append(quantity)
+    order.prices.append(price)
 
-    @property
-    def total_price(self) -> int:
-        total = 0
-        for i in range(len(self.prices)):
-            total += self.quantities[i] * self.prices[i]
-        return total
+
+def compute_total_price(order: Order) -> None:
+    total = 0
+    for i in range(len(order.prices)):
+        total += order.quantities[i] * order.prices[i]
+    print(f"The total price is: ${(total / 100):.2f}.")
+
 
 def main() -> None:
     order = Order()
-    order.add_item("Keyboard", 1, 5000)
-    order.add_item("SSD", 1, 15000)
-    order.add_item("USB cable", 2, 500)
+    add_item(order, "Keyboard", 1, 5000)
+    add_item(order, "SSD", 1, 15000)
+    add_item(order, "USB cable", 2, 500)
 
-    print(f"The total price is: ${(order.total_price / 100):.2f}.")
-
+    compute_total_price(order)
 
 
 if __name__ == "__main__":
